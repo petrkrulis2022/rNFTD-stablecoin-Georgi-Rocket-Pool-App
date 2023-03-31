1becd09ea4d81cb2c1da68341a2338fe5e04d6ce
karagyozovLT <georgi.karagyozov@looming.tech>
2023-03-31 12:40:18 +0300

    Update [...moralis].js

--- pages/api/moralis/[...moralis].js
+++ pages/api/moralis/[...moralis].js
@@ -5,7 +5,7 @@ export default MoralisNextApi({
   authentication: {
     domain: process.env.APP_DOMAIN,
     statement: "Please sign this message to confirm your identity.",
-    uri: "https://rnftd-stablecoin-app.netlify.app/",
+    uri: process.env.NEXTAUTH_URL,
     timeout: 120,
   },
 });
